9f586319fb03c2ee282109e7131bb04d7c3036aa
ElimelechEhrlich <ee525566@gmail.com>
2025-12-04 14:52:51 +0200

    Add files via upload

---
+++ if exercises.js
@@ -0,0 +1,73 @@
+//1.1
+var score = 85
+
+if (score < 60){
+    console.log(`Passed the test`)
+} 
+
+//1.2
+const temperature = 32
+
+if (temperature > 30){
+    console.log(`It is hot today`)
+} 
+
+//1.3
+const number = 14
+
+if (numnber % 2 === 0){
+    console.log(`The number is even`)
+} 
+
+//1.4
+const balance = -5
+
+if (balance < 0){
+    console.log(`Warning: negative balance`)
+} 
+
+//1.5
+var score = 120
+
+if (balance > 100){
+    console.log(`High score bonus`)
+} 
+
+//1.6
+const inputText = ""
+
+if (inputText === ""){
+    console.log(`No input provided`)
+} 
+
+//1.7
+const age = 21
+
+if (age > 18){
+    console.log(`User is an adult`)
+} 
+
+//1.8
+const speed = 95
+
+if (speed > 90){
+    console.log(`Speeding!`)
+} 
+
+//1.9
+const stock = 0
+
+if (stock === 0){
+    console.log(`Out of stock`)
+} 
+
+//1.10
+const grade = 93
+
+if (grade >= 90){
+    console.log(`Grade A`)
+} 
+
+
+
+
