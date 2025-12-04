3c5374990a1656ba53a848b797b5a12af76c5266
ElimelechEhrlich <ee525566@gmail.com>
2025-12-04 15:24:28 +0200

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,78 @@
+let num = 1
+while (num <= 5) {
+    console.log(num)
+    num++;    
+}
+
+num = 5
+while (num > 0) {
+    console.log(num)
+    num--;    
+}
+
+num = 1
+let sum = 0
+while (num <= 10) {
+    sum += num
+    num++
+}
+console.log(sum)
+
+num = 2
+while (num <= 10) {
+    console.log(num)
+    num += 2;    
+}
+
+num = 3
+while (num < 10) {
+    console.log(`num is ${num}`)
+    num += 2;    
+}
+
+num = 1
+while (num <= 3) {
+    console.log(num)
+    num ++;    
+}
+
+num = 0
+while (num <= 25) {
+    console.log(num)
+    num += 5;    
+}
+
+num = 10
+while (num > 0) {
+    console.log(num)
+    num -= 2;    
+}
+
+let counter = 1
+while (counter <= 4) {
+    console.log("hello")
+    counter++
+}
+
+num = 2
+sum = 0
+while (num <= 10) {
+    sum += num
+    num += 2;    
+}
+console.log(sum);
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (i = 5; i > 0; i--) {
+    console.log(i);
+}
+
+sum = 0
+for (i = 1; i <= 10; i++) {
+    sum += i
+}
+console.log(sum)
